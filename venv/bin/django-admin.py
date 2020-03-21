694d8010063a4ec6e71d83befd1a974976a7d904
bofomiso <maycor16@yahoo.com>
2020-03-20 21:33:18 -0700

    Django up and running

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/miguel/ChicoState/Spring20/cins465/CINS465-FinalProject/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
