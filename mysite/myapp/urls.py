54c86ada4b52e2dc1cb573ea65ce14ed85bd55fe
bofomiso <maycor16@yahoo.com>
2020-03-26 22:05:28 -0700

    added routes

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -4,5 +4,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.home),
+    path('articles/', views.articles, name = 'Articles'),
+    path('pictures/', views.pictures, name = 'Pictures'),
+    path('resume/', views.resume, name = 'Resume'),
+    path('chat/', views.chat, name = 'Chat'),
+
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World")
+def home(request):
+    return render(request, 'myapp/home.html')
+
+
+def articles(request):
+    return render(request, 'myapp/articles.html')
+
+def pictures(request):
+    return render(request, 'myapp/pictures.html')
+
+def resume(request):
+    return render(request, 'myapp/resume.html')
+
+def chat(request):
+    return render(request, 'myapp/chat.html')
