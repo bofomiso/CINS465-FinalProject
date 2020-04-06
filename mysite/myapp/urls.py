1d1433c9345986286be44b8987813bd756d5ecc2
bofomiso <maycor16@yahoo.com>
2020-04-05 23:37:00 -0700

    added more func to login and logout

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
@@ -10,5 +11,8 @@ urlpatterns = [
     path('resume/', views.resume, name = 'Resume'),
     path('chat/', views.chat, name = 'Chat'),
     path('register/', views.register, name = 'Register'),
+    path('login/', LoginView.as_view(template_name = 'myapp/login.html'), name = 'Login'),
+    path('logou/', LogoutView.as_view(template_name = 'myapp/logout.html'), name = 'Logout'),
+
 
 ]
